30521ce5678ba96ffe72810800a0db6a5e6528d3
Polina Volkhontseva <polina.volkhontseva@huawei.com>
2022-02-28 12:12:46 +0300

    Initial commit

---
+++ src/main/java/org/cqfn/astgen/Main.java
@@ -0,0 +1,15 @@
+package org.cqfn.astgen;
+
+/**
+ * Main class.
+ */
+public final class Main {
+    /**
+     * The main function. Parses the command line and runs actions.
+     *
+     * @param args the command-line arguments.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Welcome to AST Generator project!");
+    }
+}
