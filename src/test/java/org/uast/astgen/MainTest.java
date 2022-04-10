ac3c3fa2884cca3b68852d53b45519538c9f4c3c
Polina <volkhontseva.polina@gmail.com>
2022-04-10 14:46:24 +0300

    Added tests for CLI options

--- src/test/java/org/uast/astgen/MainTest.java
+++ src/test/java/org/uast/astgen/MainTest.java
@@ -24,7 +24,17 @@ public class MainTest {
     /**
      * The generate option as an argument example.
      */
-    private static final String ARG = "-g";
+    private static final String ARG = "--generate";
+
+    /**
+     * The name of the option for a root of the target project.
+     */
+    private static final String ROOT = "--root";
+
+    /**
+     * The name of the option for a package name of generated files.
+     */
+    private static final String PCG = "--package";
 
     /**
      * Test passing an option to main().
@@ -33,9 +43,7 @@ public class MainTest {
      */
     @Test
     public void testNoException(@TempDir final Path source) throws IOException {
-        final Path file = source.resolve("example.txt");
-        final List<String> lines = Collections.singletonList("Addition<-Expression, Expression;");
-        Files.write(file, lines);
+        final Path file = this.createTempDir(source);
         final String[] example = {
             MainTest.ARG,
             file.toString(),
@@ -49,13 +57,103 @@ public class MainTest {
         Assertions.assertFalse(caught);
     }
 
+    @Test
+    public void testProjectRootOptionNoException(@TempDir final Path source) throws IOException {
+        final Path file = this.createTempDir(source);
+        final String[] example = {
+            MainTest.ARG,
+            file.toString(),
+            MainTest.ROOT,
+            "test/root",
+        };
+        boolean caught = false;
+        try {
+            Main.main(example);
+        } catch (final ParameterException exc) {
+            caught = true;
+        }
+        Assertions.assertFalse(caught);
+    }
+
+    @Test
+    public void testProjectRootOptionWithException(@TempDir final Path source) throws IOException {
+        final Path file = this.createTempDir(source);
+        final String[] example = {
+            MainTest.ARG,
+            file.toString(),
+            MainTest.ROOT,
+            "test/ro:ot",
+        };
+        boolean caught = false;
+        try {
+            Main.main(example);
+        } catch (final ParameterException exc) {
+            caught = true;
+        }
+        Assertions.assertTrue(caught);
+    }
+
+    @Test
+    public void testProjectRootOptionWithExistingPath(@TempDir final Path source)
+        throws IOException {
+        final Path file = this.createTempDir(source);
+        final String[] example = {
+            MainTest.ARG,
+            file.toString(),
+            MainTest.ROOT,
+            source.toString(),
+        };
+        boolean caught = false;
+        try {
+            Main.main(example);
+        } catch (final ParameterException exc) {
+            caught = true;
+        }
+        Assertions.assertFalse(caught);
+    }
+
+    @Test
+    public void testPackageOptionNoException(@TempDir final Path source) throws IOException {
+        final Path file = this.createTempDir(source);
+        final String[] example = {
+            MainTest.ARG,
+            file.toString(),
+            MainTest.PCG,
+            "org.uast",
+        };
+        boolean caught = false;
+        try {
+            Main.main(example);
+        } catch (final ParameterException exc) {
+            caught = true;
+        }
+        Assertions.assertFalse(caught);
+    }
+
+    @Test
+    public void testPackageOptionWithException(@TempDir final Path source) throws IOException {
+        final Path file = this.createTempDir(source);
+        final String[] example = {
+            MainTest.ARG,
+            file.toString(),
+            MainTest.PCG,
+            "org/uast",
+        };
+        boolean caught = false;
+        try {
+            Main.main(example);
+        } catch (final ParameterException exc) {
+            caught = true;
+        }
+        Assertions.assertTrue(caught);
+    }
+
     /**
      * Test passing no option to main().
      */
     @Test
     public void testWithException() {
-        final String[] example = {
-        };
+        final String[] example = {};
         boolean caught = false;
         try {
             Main.main(example);
@@ -69,7 +167,7 @@ public class MainTest {
      * Test passing an option with no parameters to main().
      */
     @Test
-    public void testMain() throws IOException {
+    public void testMain() {
         final String[] example = {
             MainTest.ARG,
         };
@@ -82,6 +180,13 @@ public class MainTest {
             message = exc.getMessage();
         }
         Assertions.assertTrue(caught);
-        Assertions.assertEquals("Expected a value after parameter -g", message);
+        Assertions.assertEquals("Expected a value after parameter --generate", message);
+    }
+
+    private Path createTempDir(@TempDir final Path source) throws IOException {
+        final Path file = source.resolve("example.txt");
+        final List<String> lines = Collections.singletonList("Addition<-Expression, Expression;");
+        Files.write(file, lines);
+        return file;
     }
 }
