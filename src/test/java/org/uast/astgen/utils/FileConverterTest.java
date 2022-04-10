c625e18a82aa96710f97828ea5ddd779b43cee6f
Polina <volkhontseva.polina@gmail.com>
2022-04-10 16:40:48 +0300

    Refactoring of CLI tests

---
+++ src/test/java/org/uast/astgen/utils/FileConverterTest.java
@@ -0,0 +1,70 @@
+/*
+ * MIT License Copyright (c) 2022 unified-ast
+ * https://github.com/unified-ast/ast-generator/blob/master/LICENSE.txt
+ */
+package org.uast.astgen.utils;
+
+import com.beust.jcommander.ParameterException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.uast.astgen.Main;
+
+/**
+ * Test for {@link FileConverter} class exception cases.
+ *
+ * @since 1.0
+ */
+public class FileConverterTest {
+    /**
+     * The generate option as an argument example.
+     */
+    private static final String ARG = "--generate";
+
+    /**
+     * Test passing the {@code --generate} option to main() with file
+     * which has an irrelevant extension.
+     * @param source A temporary directory
+     */
+    @Test
+    public void testWrongFileExtension(@TempDir final Path source) throws IOException {
+        final Path file = source.resolve("example.java");
+        final List<String> lines = Collections.singletonList("class A{}");
+        Files.write(file, lines);
+        final String[] example = {
+            FileConverterTest.ARG,
+            file.toString(),
+        };
+        boolean caught = false;
+        try {
+            Main.main(example);
+        } catch (final ParameterException exc) {
+            caught = true;
+        }
+        Assertions.assertTrue(caught);
+    }
+
+    /**
+     * Test passing the {@code --generate} option to main() with file
+     * which does not exist.
+     */
+    @Test
+    public void testNotExistingFile() throws IOException {
+        final String[] example = {
+            FileConverterTest.ARG,
+            "some/file.txt",
+        };
+        boolean caught = false;
+        try {
+            Main.main(example);
+        } catch (final ParameterException exc) {
+            caught = true;
+        }
+        Assertions.assertTrue(caught);
+    }
+}
