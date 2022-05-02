9c6d94d32d1e1a7cbae14140229d0a9648334add
Ivan Kniazkov <ivan@kniazkov.com>
2022-05-02 08:33:31 +0300

    Matcher compares and extracts data

--- src/main/java/org/uast/astgen/interpreter/Matcher.java
+++ src/main/java/org/uast/astgen/interpreter/Matcher.java
@@ -6,7 +6,10 @@ package org.uast.astgen.interpreter;
 
 import java.util.Map;
 import org.uast.astgen.base.Node;
+import org.uast.astgen.rules.Data;
 import org.uast.astgen.rules.Descriptor;
+import org.uast.astgen.rules.Hole;
+import org.uast.astgen.rules.StringData;
 
 /**
  * Matcher that works with the raw descriptor.
@@ -30,7 +33,8 @@ public final class Matcher implements org.uast.astgen.base.Matcher {
     @Override
     public boolean match(final Node node, final Map<Integer, Node> children,
         final Map<Integer, String> data) {
-        return this.checkType(node) && this.checkChildCount(node);
+        return this.checkType(node) && this.checkChildCount(node)
+            && this.checkAndExtractData(node, data);
     }
 
     /**
@@ -49,5 +53,25 @@ public final class Matcher implements org.uast.astgen.base.Matcher {
      */
     private boolean checkChildCount(final Node node) {
         return node.getChildCount() == this.descriptor.getParameters().size();
+    }
+
+    /**
+     * Checks the data matches, extracts the data.
+     * @param node The node
+     * @param extracted The collection for saving extracted data
+     * @return Checking result, {@code true} if the data matches
+     */
+    private boolean checkAndExtractData(final Node node, final Map<Integer, String> extracted) {
+        final Data data = this.descriptor.getData();
+        final boolean result;
+        if (data instanceof StringData) {
+            result = node.getData().equals(((StringData) data).getValue());
+        } else if (data instanceof Hole) {
+            extracted.put(((Hole) data).getValue(), node.getData());
+            result = true;
+        } else {
+            result = node.getData().isEmpty();
+        }
+        return result;
     }
 }
--- src/test/java/org/uast/astgen/interpreter/MatcherTest.java
+++ src/test/java/org/uast/astgen/interpreter/MatcherTest.java
@@ -5,12 +5,16 @@
 package org.uast.astgen.interpreter;
 
 import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.uast.astgen.base.DraftNode;
 import org.uast.astgen.base.Node;
 import org.uast.astgen.rules.Descriptor;
 import org.uast.astgen.rules.DescriptorFactory;
+import org.uast.astgen.rules.Hole;
+import org.uast.astgen.rules.StringData;
 import org.uast.astgen.utils.LabelFactory;
 
 /**
@@ -34,5 +38,48 @@ public class MatcherTest {
         final Matcher matcher = new Matcher(descriptor);
         final boolean result = matcher.match(node, Collections.emptyMap(), Collections.emptyMap());
         Assertions.assertTrue(result);
+    }
+
+    /**
+     * Testing the case when the descriptor contains a data represented as a string.
+     */
+    @Test
+    public void testDataMatching() {
+        final String type = "literal";
+        final String data = "+";
+        final DraftNode.Constructor ctor = new DraftNode.Constructor();
+        ctor.setName(type);
+        ctor.setData(data);
+        final Node node = ctor.createNode();
+        final LabelFactory labels = new LabelFactory();
+        final DescriptorFactory factory = new DescriptorFactory(labels.getLabel(), type);
+        factory.setData(new StringData(data));
+        final Descriptor descriptor = factory.createDescriptor();
+        final Matcher matcher = new Matcher(descriptor);
+        final boolean result = matcher.match(node, Collections.emptyMap(), Collections.emptyMap());
+        Assertions.assertTrue(result);
+    }
+
+    /**
+     * Testing the case when the descriptor contains a data represented as a hole.
+     */
+    @Test
+    public void testDataExtracting() {
+        final String type = "identifier";
+        final String data = "x";
+        final DraftNode.Constructor ctor = new DraftNode.Constructor();
+        ctor.setName(type);
+        ctor.setData(data);
+        final Node node = ctor.createNode();
+        final LabelFactory labels = new LabelFactory();
+        final DescriptorFactory factory = new DescriptorFactory(labels.getLabel(), type);
+        factory.setData(new Hole(0));
+        final Descriptor descriptor = factory.createDescriptor();
+        final Matcher matcher = new Matcher(descriptor);
+        final Map<Integer, String> collection = new TreeMap<>();
+        final boolean result = matcher.match(node, Collections.emptyMap(), collection);
+        Assertions.assertTrue(result);
+        Assertions.assertTrue(collection.containsKey(0));
+        Assertions.assertEquals(data, collection.get(0));
     }
 }
