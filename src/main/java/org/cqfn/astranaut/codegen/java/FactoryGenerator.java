0203e822318e89ebfec56b945fca6ae1c074cfeb
Ivan Kniazkov <ivan@kniazkov.com>
2025-02-07 13:40:05 +0300

    Fixed factory generator

--- src/main/java/org/cqfn/astranaut/codegen/java/FactoryGenerator.java
+++ src/main/java/org/cqfn/astranaut/codegen/java/FactoryGenerator.java
@@ -23,8 +23,11 @@
  */
 package org.cqfn.astranaut.codegen.java;
 
-import java.util.Collection;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 import org.cqfn.astranaut.dsl.AbstractNodeDescriptor;
 import org.cqfn.astranaut.dsl.NodeDescriptor;
 import org.cqfn.astranaut.dsl.Program;
@@ -81,7 +84,7 @@ public class FactoryGenerator {
         instance.makeFinal(String.format("new %s()", classname));
         klass.addField(instance);
         FactoryGenerator.createMapOfProperties(language, klass);
-        this.createMapOfTypes(language, klass);
+        final Set<NodeDescriptor> dependencies = this.createMapOfTypes(language, klass);
         final CompilationUnit unit = new CompilationUnit(
             context.getLicense(),
             context.getPackage(),
@@ -92,6 +95,16 @@ public class FactoryGenerator {
         unit.addImport("org.cqfn.astranaut.core.base.Factory");
         unit.addImport("org.cqfn.astranaut.core.base.Type");
         unit.addImport("org.cqfn.astranaut.core.utils.MapUtils");
+        for (final NodeDescriptor dependency : dependencies) {
+            if (!dependency.getLanguage().equals(language)) {
+                final Package pkg = context
+                    .getPackage()
+                    .getParent()
+                    .getParent()
+                    .getSubpackage(dependency.getLanguage(), "nodes");
+                unit.addImport(String.format("%s.%s", pkg.toString(), dependency.getName()));
+            }
+        }
         return unit;
     }
 
@@ -127,8 +140,10 @@ public class FactoryGenerator {
      * Creates a collection that contains types supported by this factory.
      * @param language Language for which the factory is generated
      * @param klass The class to which to add the field
+     * @return Dependencies Set of descriptors to be imported
      */
-    private void createMapOfTypes(final String language, final Klass klass) {
+    private Set<NodeDescriptor> createMapOfTypes(final String language, final Klass klass) {
+        final Set<NodeDescriptor> dependencies = new HashSet<>();
         final Field field = new Field(
             "Map<String, Type>",
             "TYPES",
@@ -138,9 +153,17 @@ public class FactoryGenerator {
         field.makeStatic();
         final StringBuilder builder = new StringBuilder(128);
         builder.append("new MapUtils<String, Type>()");
-        final Collection<NodeDescriptor> rules =
-            this.program.getNodeDescriptorsByLanguage(language).values();
-        for (final NodeDescriptor rule : rules) {
+        final Map<String, NodeDescriptor> rules;
+        if (language.equals("common")) {
+            rules = this.program.getNodeDescriptorsByLanguage(language);
+        } else {
+            rules = new TreeMap<>(this.program.getNodeDescriptorsByLanguage(language));
+            final Map<String, NodeDescriptor> common =
+                this.program.getNodeDescriptorsByLanguage("common");
+            rules.putAll(common);
+        }
+        for (final Map.Entry<String, NodeDescriptor> entry : rules.entrySet()) {
+            final NodeDescriptor rule = entry.getValue();
             if (!(rule instanceof AbstractNodeDescriptor)) {
                 builder
                     .append(".put(")
@@ -148,10 +171,12 @@ public class FactoryGenerator {
                     .append(".NAME, ")
                     .append(rule.getName())
                     .append(".TYPE)");
+                dependencies.add(rule);
             }
         }
         builder.append(".make()");
         field.makeFinal(builder.toString());
         klass.addField(field);
+        return dependencies;
     }
 }
