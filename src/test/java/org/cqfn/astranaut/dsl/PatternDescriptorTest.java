eebb757f13065129cc5d8418146625ff093d2bf3
Ivan Kniazkov <ivan@kniazkov.com>
2025-05-15 19:43:18 +0300

    + test

---
+++ src/test/java/org/cqfn/astranaut/dsl/PatternDescriptorTest.java
@@ -0,0 +1,85 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2025 Ivan Kniazkov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cqfn.astranaut.dsl;
+
+import java.util.Arrays;
+import java.util.Collections;
+import org.cqfn.astranaut.core.algorithms.conversion.Extracted;
+import org.cqfn.astranaut.core.base.DraftNode;
+import org.cqfn.astranaut.core.base.Node;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests covering {@link PatternDescriptor} class.
+ * @since 1.0.0
+ */
+class PatternDescriptorTest {
+    @Test
+    void matchNodeWithChildrenAndPatternWithout() {
+        final PatternDescriptor pattern = new PatternDescriptor(
+            "A",
+            null,
+            Collections.emptyList()
+        );
+        final Node node = DraftNode.create("A(B,C)");
+        boolean matches = pattern.matchNode(node, new Extracted());
+        Assertions.assertFalse(matches);
+    }
+
+    @Test
+    void matchNodeWithoutChildrenAndPatternWith() {
+        final PatternDescriptor pattern = new PatternDescriptor(
+            "A",
+            null,
+            Arrays.asList(
+                new PatternDescriptor("B", null, Collections.emptyList()),
+                new PatternDescriptor("C", null, Collections.emptyList())
+            )
+        );
+        final Node node = DraftNode.create("A");
+        boolean matches = pattern.matchNode(node, new Extracted());
+        Assertions.assertFalse(matches);
+    }
+
+    @Test
+    void matchPatternWithHole() {
+        final PatternDescriptor pattern = new PatternDescriptor(
+            "C",
+            null,
+            Arrays.asList(
+                UntypedHole.getInstance(1),
+                UntypedHole.getInstance(2)
+            )
+        );
+        final Node node = DraftNode.create("C(D,E)");
+        final Extracted extracted = new Extracted();
+        boolean matches = pattern.matchNode(node, extracted);
+        Assertions.assertTrue(matches);
+        Assertions.assertFalse(extracted.getNodes(1).isEmpty());
+        Assertions.assertEquals("D", extracted.getNodes(1).get(0).getTypeName());
+        Assertions.assertFalse(extracted.getNodes(2).isEmpty());
+        Assertions.assertEquals("E", extracted.getNodes(2).get(0).getTypeName());
+    }
+}
